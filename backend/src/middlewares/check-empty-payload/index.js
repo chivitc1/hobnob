79fc81c82fb03260da10ca0250ee314827b481b8
chivitc1 <chivitc1@gmail.com>
2018-12-31 23:20:11 +0700

    Add unit tests for checkEmptyPayload middleware

--- backend/src/middlewares/check-empty-payload/index.js
+++ backend/src/middlewares/check-empty-payload/index.js
@@ -1,9 +1,8 @@
 function checkEmptyPayLoad(req, res, next) {
-  if (['POST', 'PUT', 'PATCH'].includes(req.method) && req.headers['content-length'] == 0) {
-    res.status(400)
-    res.set({'Content-Type': 'application/json'});
-    res.json({message: 'Payload should not be empty'});
-    return;
+  if (['POST', 'PUT', 'PATCH'].includes(req.method) && req.headers['content-length'] === '0') {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    return res.json({message: 'Payload should not be empty'});
   }
   next();
 }
